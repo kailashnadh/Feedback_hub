ddfb2d69db5850253b2e5748d995d2f6fe15b532
kailashnadh.mamidi <mkailashnadh@gmail.com>
2020-06-02 13:56:13 -0300

    added api

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express=require('express');
+
+const cookie=require('cookie-session');
+const passport=require('passport');
+const mongoose=require('mongoose');
+const keys=require('./config/keys');
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.send({hi :'there'})
-});
+app.use(
+    cookie({
+        maxAge:30*24*60*60*1000,
+        keys:[keys.cookieKey]
+    })
+)
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./models/Users');
+require('./services/passport');
+mongoose.connect(keys.mongoURI);
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,44 @@
+const keys= require('../config/keys');
+
+const passport=require('passport');
+const googleStrategy=require('passport-google-oauth20').Strategy;
+const mongoose=require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user,done)=>{
+    done(null,user.id)
+})
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id).then(
+        (user)=>{
+            done(null,user)
+        }
+    )
+})
+
+
+passport.use(new googleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+},
+(accesstoken, refreshtoken, profile, done) =>{
+
+    User.findOne({googleId: profile.id}).then(
+        existingUser => {
+            if(existingUser){
+                done(null,existingUser);
+            }else{
+                new User({googleId:profile.id}).save().then(
+                    user =>{
+                        done(null,user);
+                    }
+                );
+            }
+        }
+    )
+
+    
+}))
